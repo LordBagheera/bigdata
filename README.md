dbe28c34c91d0cd48581135eea84ba158977f89b
lord-bagheera <erik.hurtado.sepulveda@nttdata.com>
2022-07-31 00:12:23 -0400

    dev @ 31/07

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bigdata
 Pseudo cluster for big data operations, simulating enterprise system
+
+Thanks to BDE2020 for base images and documentation-
--- spark-base/image_env_setup.sh
+++ spark-base/image_env_setup.sh
@@ -1,8 +1,7 @@
-#export SPARK_HOME=/opt/spark
-#export HADOOP_HOME=/opt/hadoop
+export SPARK_HOME=/spark
+export HADOOP_HOME=/opt/hadoop
 #export CORE_CONF_fs_defaultFS=hdfs://namenode:8020
 #export CORE_CONF_hadoop_http_staticuser_user=root
-#export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
-export SBT_HOME=/opt/sbt/
-#export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:HADOOP_HOME/sbin:$SBT_HOME/bin
-export PATH=$PATH:$SBT_HOME/bin
+export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
+export SBT_HOME=/opt/sbt
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SBT_HOME/bin

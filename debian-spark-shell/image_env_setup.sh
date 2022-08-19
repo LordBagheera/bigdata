f62e321129e9cb16b5d791381824da01adeaaa1b
LordBagheera <erik.hurtado@inacapmail.cl>
2022-08-19 01:19:23 -0400

    v0.911 afinando y limpiando cluster

--- debian-spark-shell/image_env_setup.sh
+++
@@ -1,7 +0,0 @@
-export SPARK_HOME=opt/spark
-export HADOOP_HOME=/opt/hadoop
-export CORE_CONF_fs_defaultFS=hdfs://namenode:8020
-export CORE_CONF_hadoop_http_staticuser_user=root
-export JAVA_HOME=/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64
-export SBT_HOME=/opt/sbt/
-export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:HADOOP_HOME/sbin:$SBT_HOME/bin

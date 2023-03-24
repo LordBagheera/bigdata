94a54444a001c6d2b329d24f3603f7a7696e7ad6
LordBagheera <erik.hurtado.sepulveda@nttdata.com>
2023-03-24 15:44:48 -0300

    Limpiando y ordenando codigo

---
+++ Limpieza/spark-client/conf/image_env_setup.sh
@@ -0,0 +1,10 @@
+export SPARK_HOME=/spark
+export HADOOP_HOME=/opt/hadoop
+#export CORE_CONF_fs_defaultFS=hdfs://namenode:8020
+#export CORE_CONF_hadoop_http_staticuser_user=root
+export JAVA_HOME=/usr/lib/jvm/java-1.8-openjdk
+export SBT_HOME=/opt/sbt
+export CORE_CONF_DIR=$HADOOP_HOME/etc/hadoop/
+export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop/
+export YARN_CONF_DIR=$HADOOP_HOME/etc/hadoop/
+export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$SBT_HOME/bin
